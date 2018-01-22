762012e5987d753f83a599e1ab013d382a04af33
Victória Aiello <vaiello@rlicorp.com>
2018-01-21 23:55:19 -0200

    success && delete

--- controller/gretchen.js
+++ controller/gretchen.js
@@ -2,8 +2,8 @@ const db     = require('../config/database');
 var gretchen = {}
 
 gretchen.list = function(request, response) {	
-	db.query('SELECT * FROM `my-gifs`', function(error, result){		
-        var id = Math.floor(Math.random() * 2) + 0;
+	db.query('SELECT COUNT (*) AS total FROM `my-gifs`', function(error, result) {
+        var id = Math.floor(Math.random() * result[0].total) + 1;
     	response.render('home', {id: id});
     });
 };
@@ -15,27 +15,26 @@ gretchen.listAll = function(request, response) {
 };
 
 gretchen.detalhe = function(request, response) {	
-	db.query('SELECT * FROM `my-gifs` WHERE id='+request.params.gifId, function(error, result){		
+	db.query('SELECT * FROM `my-gifs` WHERE id='+request.params.gifId, function(error, result){
 		const item = result[0];
-		console.log(item);
 	    response.render('detail', item);
     });
 };
 
 gretchen.adicionar = function(request, responseponse) {
 	var gif = {
-        nome  : request.body.phrase,
-        email : request.body.image
+        phrase  : request.body.phrase,
+        image : request.body.image
     }
 
-    db.query('INSERT INTO my-gifs SET ?', gif, function(error, responseult){
-        response.render('home', {result:result});        
+    db.query('INSERT INTO `my-gifs` SET ?', gif, function(error, responseult){
+        response.render('list', {result:result, success: true});        
     });	
 };
 
 gretchen.deletar = function(request, response) {
-    db.query('DELETE FROM veiculos WHERE id= '+request.params.gifId, function(error, result){           
-        response.render('home', {result:result});        
+    db.query('DELETE FROM `my-gifs` WHERE id= '+request.params.gifId, function(error, result){           
+        response.render('list', {result:result, delete: true});        
     });	
 };
 
