c6709c3b73c4457f54468679452ba00879305ebf
Victória Aiello <vaiello@rlicorp.com>
2018-01-21 23:18:28 -0200

    views e rotas

--- server.js
+++ server.js
@@ -4,11 +4,27 @@ const routes 	 = require('./config/router');
 const exphbs 	 = require('express-handlebars');
 const app        = express();
 
+const result = [
+	{image: 'alou', phrase: 'alooou1', id: 0},
+	{image: 'alou', phrase: 'alooou2', id: 1},
+	{image: 'alou', phrase: 'alooou3', id: 2}
+];
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 
-routes(app);
+app.get('/', function (req, res) {
+	let id = Math.floor(Math.random() * 2) + 0;
+    res.render('home', {id: id});
+});
+
+app.get('/result/:id', function (req, res) {
+	const item = result.filter(function (el) {return (el.id == req.params.id);});
+    res.render('detail', item[0]);
+});
+
+app.get('/new/', function (req, res) {
+    res.render('list', {result: result});
+});
 
 app.listen(3000);
